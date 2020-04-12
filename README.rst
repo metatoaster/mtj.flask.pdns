ef0a1bc4ab6a3f519f911459efdb32defcf259f9
Tommy Yu <y@metatoaster.com>
2020-04-13 01:45:19 +1200

    initial boilerplate

---
+++ README.rst
@@ -0,0 +1,7 @@
+mtj.flask.pdns
+==============
+
+A very simple Flask front-end for PDNS.  Requires a database backend.
+
+Only a single update endpoint is provided to update a specific existing
+record in the records table.
---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+classifiers = """
+Development Status :: 4 - Beta
+Environment :: Web Environment
+Framework :: Flask
+Intended Audience :: Developers
+License :: OSI Approved :: GNU Affero General Public License v3
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+""".strip().splitlines()
+
+setup(
+    name='mtj.flask.pdns',
+    version=version,
+    description="A simple flask endpoint interface to pdns",
+    long_description=(
+        open('README.rst').read() + "\n" +
+        open('CHANGES.rst').read()
+    ),
+    classifiers=classifiers,
+    keywords='',
+    author='Tommy Yu',
+    author_email='y@metatoaster.com',
+    url='https://github.com/metatoaster/mtj.flask.pdns',
+    license='AGPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    namespace_packages=['mtj', 'mtj.flask'],
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    include_package_data=True,
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*',
+    entry_points={},
+    test_suite="mtj.flask.pdns.tests.make_suite",
+)
